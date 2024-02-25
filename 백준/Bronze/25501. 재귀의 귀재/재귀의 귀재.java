2d8ee8c09ee805a68c9f25bae6370710707aa45c
ParkSuCheol <61348652+ParkSuCheol@users.noreply.github.com>
2024-02-25 23:37:52 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 328 ms, Memory: 20256 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int cnt;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++){
+            String s = br.readLine();
+            cnt = 0;
+            System.out.println(isPalindrome(s) + " " + cnt);
+        }
+        bw.flush();
+        bw.close();;
+        br.close();
+    }
+    public static int recursion(String s, int l, int r){
+        cnt++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+}
