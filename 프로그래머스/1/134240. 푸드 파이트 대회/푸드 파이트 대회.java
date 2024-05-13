2caea7e9f1c687ee0e3a07828ac85bc2e9471401
ParkSuCheol <61348652+ParkSuCheol@users.noreply.github.com>
2024-05-13 23:51:52 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 12.04 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for(int i = 1; i < food.length; i++){
+            for(int j = 0; j < food[i] / 2; j++){
+                answer += i;
+            }    
+        }
+        StringBuilder sb = new StringBuilder(answer);
+        
+        answer += 0;
+        answer += sb.reverse();
+        
+        return answer;
+    }
+}
