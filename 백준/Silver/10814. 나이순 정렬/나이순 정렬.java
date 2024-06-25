b70365aa2f03b0d5095fb931b1a962cc914fe1a0
ParkSuCheol <61348652+ParkSuCheol@users.noreply.github.com>
2024-06-25 16:52:59 +0900

    [Silver V] Title: 나이순 정렬, Time: 744 ms, Memory: 52732 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static class Person {
+        int age;
+        String name;
+
+        public Person (int age, String name){
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public String toString(){
+            return age + " " + name + '\n';
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Person[] p = new Person[N];
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            p[i] = new Person(age, name);
+        }
+
+        Arrays.sort(p, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.age - o2.age;
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            sb.append(p[i]);
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
