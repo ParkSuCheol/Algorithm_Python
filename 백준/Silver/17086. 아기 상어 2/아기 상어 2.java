ec142ca8ca0f949e47ace5aa9743577dc50be652
ParkSuCheol <61348652+ParkSuCheol@users.noreply.github.com>
2024-08-10 21:51:09 +0900

    [Silver II] Title: 아기 상어 2, Time: 368 ms, Memory: 172576 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -1,89 +1,72 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, result;
-	
-	static int[][] map;
-	static boolean[][] visited;
-	
-	// 상하좌우 대각
-	static int[] dx = {-1, -1, 0, 1, 1,  1,  0, -1};
-	static int[] dy = { 0,  1, 1, 1, 0, -1, -1, -1};
-	
-	// 좌표를 담을 클래스
-	public static class Node {
-		int x, y, dist;
-		public Node(int x, int y, int dist) {
-			this.x = x;
-			this.y = y;
-			this.dist = dist;
-		}
-	}
-	
-	// 탐색
-	public static int bfs(int x, int y) {
-		Queue<Node> q = new LinkedList<>();
-		q.offer(new Node(x, y, 0));
-		visited[x][y] = true;
-		
-		while(!q.isEmpty()) {
-			Node now = q.poll();
-			
-			for(int i = 0; i < 8; i++) {
-				int nextX = now.x + dx[i];
-				int nextY = now.y + dy[i];
-				
-				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
-					continue;
-				}
-				
-				if(visited[nextX][nextY] == true) {
-					continue;
-				}
-				
-				if(map[nextX][nextY] == 1) {
-					return now.dist + 1;
-				}
-				
-				visited[nextX][nextY] = true;
-				q.offer(new Node(nextX, nextY, now.dist + 1));
-			}
-		}
-		return 0;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		visited = new boolean[N][M];
-		
-		// 맵 입력
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		result = 0;
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				if(map[i][j] == 0) {
-					visited = new boolean[N][M];
-					result = Math.max(result, bfs(i, j));
-				}
-			}
-		}
-		
-		System.out.println(result);
-	}
+    static int[][] map;
+    static boolean[][] v;
+    static int N, M;
+    static int[] dx = {-1, 0, 1, -1, 1, -1, 0, 1};	//이동관련 X 변경값
+    static int[] dy = {-1, -1, -1, 0, 0, 1, 1, 1};	//이동관련 y 변경값
+
+    static class Point{
+        int x, y, dist;
+        public Point(int x, int y, int dist){
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        v = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int res = Integer.MIN_VALUE;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                if(map[i][j] == 0) {
+                    v = new boolean[N][M];
+                    res = Math.max(res, bfs(i,j));
+                }
+            }
+        }
+        System.out.println(res);
+    }
+
+    public static int bfs(int x, int y){
+        Queue<Point> q = new LinkedList<>();
+        v[x][y] = true;
+        q.add(new Point(x,y,0));
+        while(!q.isEmpty()){
+            Point cur = q.poll();
+            for(int i = 0; i < 8; i++){
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M)
+                    continue;
+                if(v[nx][ny])
+                    continue;
+                if(map[nx][ny] == 1)
+                    return cur.dist+1;
+                v[nx][ny] = true;
+                q.add(new Point(nx,ny,cur.dist + 1));
+            }
+        }
+        return 0;
+    }
 }
