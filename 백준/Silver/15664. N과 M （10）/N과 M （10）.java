4ad0aa3ffcc03c2b6bf50ddd19a5e41f0f096c04
ParkSuCheol <61348652+ParkSuCheol@users.noreply.github.com>
2024-09-06 21:39:12 +0900

    [Silver II] Title: N과 M (10), Time: 100 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static LinkedHashSet<String> set = new LinkedHashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int[] output = new int[N];
+        boolean[] v = new boolean[N];
+
+        combination(arr, output, v, 0, 0, M);
+
+        for (String s : set){
+            System.out.println(s);
+        }
+    }
+
+    public static void combination(int[] arr, int[] output, boolean[] v, int depth, int start, int end){
+        if(depth == end){
+            sb = new StringBuilder();
+            for (int i = 0; i < end; i++){
+                sb.append(output[i]).append(" ");
+            }
+            set.add(sb.toString());
+            return;
+        }
+        for(int i = start; i < arr.length; i++){
+            if (!v[i]){
+                v[i] = true;
+                output[depth] = arr[i];
+                combination(arr, output, v, depth+1, i, end);
+                v[i] = false;
+            }
+        }
+    }
+}
