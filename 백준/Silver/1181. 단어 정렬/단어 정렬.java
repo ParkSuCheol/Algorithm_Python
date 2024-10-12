f82ebf1bd658314ab26b93c1c98f5409534c51d1
ParkSuCheol <61348652+ParkSuCheol@users.noreply.github.com>
2024-10-12 14:58:58 +0900

    [Silver V] Title: 단어 정렬, Time: 288 ms, Memory: 26240 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,23 +1,23 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-        
+        int N = Integer.parseInt(st.nextToken());
         String[] arr = new String[N];
-        
-        for (int i = 0; i < N; i++) {
+
+        for(int i = 0; i < N; i++){
             arr[i] = br.readLine();
         }
-        
-        Arrays.sort(arr, new Comparator<String>() {
-            public int compare(String o1, String o2) {
+
+        Arrays.sort(arr, new Comparator<String>(){
+            public int compare(String o1, String o2){
                 if(o1.length() == o2.length()){
                     return o1.compareTo(o2);
                 }
@@ -26,19 +26,17 @@ public class Main {
                 }
             }
         });
-        
+
         StringBuilder sb = new StringBuilder();
-        
         sb.append(arr[0]).append('\n');
-        
+
         for(int i = 1; i < N; i++){
-            if(!arr[i].equals(arr[i-1]))
+            if(!arr[i].equals(arr[i-1])){
                 sb.append(arr[i]).append('\n');
+            }
         }
-        
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb.toString());
+
     }
+
 }
