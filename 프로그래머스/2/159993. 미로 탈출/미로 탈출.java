85aa7c5754f76cf2d6294901a7515a170e5e6183
ParkSuCheol <61348652+ParkSuCheol@users.noreply.github.com>
2024-10-17 22:47:58 +0900

    [level 2] Title: 미로 탈출, Time: 10.58 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,98 +1,77 @@
 import java.util.*;
-
 class Solution {
     static char[][] map;
     static boolean[][] v;
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
     static int N, M;
-
     public static int solution(String[] maps) {
         int answer = 0;
-        M = maps.length; // 행의 개수
-        N = maps[0].length(); // 열의 개수
-        map = new char[M][N]; // 배열 크기 설정
-        v = new boolean[M][N];
-
-        for (int i = 0; i < M; i++) {
+        N = maps.length;
+        M = maps[0].length();
+        
+        map = new char[N][M];
+        v = new boolean[N][M];
+        
+        for(int i = 0; i < N; i++){
             String tmp = maps[i];
-            for (int j = 0; j < N; j++) {
+            for(int j = 0; j < M; j++){
                 map[i][j] = tmp.charAt(j);
             }
         }
-
-        int result = 0;
-
-        // 'S'에서 'L'까지의 거리
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 'S') {
-                    int distanceL = bfs(i, j, 'L');
-                    if (distanceL == -1) {
+        
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                if (map[i][j] == 'S'){
+                    int frst = bfs(i,j,'L');
+                    if(frst == -1)
                         return -1;
-                    }
-                    result += distanceL;
+                    else
+                        answer += frst;
                 }
             }
         }
-
-        // 방문 배열 초기화
-        v = new boolean[M][N];
-
-        // 'L'에서 'E'까지의 거리
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 'L') {
-                    int distanceE = bfs(i, j, 'E');
-                    if (distanceE == -1) {
+        v = new boolean[N][M];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                if(map[i][j] == 'L'){
+                    int sec = bfs(i,j,'E');
+                    if(sec == -1)
                         return -1;
-                    }
-                    result += distanceE;
+                    else
+                        answer += sec;
                 }
             }
         }
-
-        return result;
+        return answer;
     }
-
-    public static int bfs(int x, int y, char target) {
+    
+    public static int bfs(int x, int y, char target){
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {x, y});
+        q.add(new int[]{x, y, 0});
         v[x][y] = true;
-        int cnt = 0;
-
-        // BFS 시작
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int[] tmp = q.poll();
-                int fx = tmp[0];
-                int fy = tmp[1];
-
-                // 4방향 탐색
-                for (int d = 0; d < 4; d++) {
-                    int nx = fx + dx[d];
-                    int ny = fy + dy[d];
-
-                    // 경계 체크
-                    if (nx < 0 || nx >= M || ny < 0 || ny >= N) {
-                        continue;
-                    }
-
-                    // 목표를 찾았을 때
-                    if (!v[nx][ny] && map[nx][ny] == target) {
-                        return cnt + 1; // 현재 거리 반환
-                    }
-
-                    // 'O'일 때 큐에 추가
-                    if (!v[nx][ny] && map[nx][ny] != 'X') {
-                        v[nx][ny] = true;
-                        q.add(new int[] {nx, ny});
-                    }
+        while(!q.isEmpty()){
+            int[] tmp = q.poll();
+            int nx = tmp[0];
+            int ny = tmp[1];
+            int cnt = tmp[2];
+            for(int i = 0; i < 4; i++){
+                int fx = nx+dx[i];
+                int fy = ny+dy[i];
+                if(fx < 0 || fx >= N || fy < 0 || fy >= M)
+                    continue;
+                if(map[fx][fy] == target){
+                    v[fx][fy] = true;
+                    return cnt+1;
                 }
+                if(v[fx][fy] || map[fx][fy] == 'X')
+                    continue;
+                
+                q.add(new int[]{fx,fy,cnt+1});
+                v[fx][fy] = true;
             }
-            cnt++; // 한 레벨 탐색이 끝나면 거리 증가
         }
-        return -1; // 목표를 찾지 못한 경우
+        
+        return -1;
     }
 }
