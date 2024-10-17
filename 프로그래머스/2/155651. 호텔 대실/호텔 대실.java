dc20a0e0b2cdb927f16c83e5875d337512c01707
ParkSuCheol <61348652+ParkSuCheol@users.noreply.github.com>
2024-10-17 23:53:44 +0900

    [level 2] Title: 호텔 대실, Time: 12.47 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -2,23 +2,27 @@ import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
-        int[] res = new int[3000];
+        ArrayList<int[]> list = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i = 0; i < book_time.length; i++){
             String start = book_time[i][0];
             String end = book_time[i][1];
             int startTime = Integer.parseInt(start.split(":")[0]) * 60 + Integer.parseInt(start.split(":")[1]);
             int endTime = Integer.parseInt(end.split(":")[0]) * 60 + Integer.parseInt(end.split(":")[1]);
-            for(int j = startTime; j < endTime+10; j++){
-                res[j]++;
+            list.add(new int[]{startTime, endTime+10});
+        }
+        Collections.sort(list, (o1, o2)->{
+           return o1[0] - o2[0]; 
+        });
+        //System.out.println(Arrays.toString(res));
+        
+        for(int[] t : list){
+            if(!pq.isEmpty() && pq.peek() <= t[0]){
+                pq.poll();
             }
+            pq.add(t[1]);
         }
         
-        //System.out.println(Arrays.toString(res));
-        int max = 0;
-        for(int i = 0; i < res.length; i++){
-            if (max < res[i])
-                max = res[i];
-        }
-        return max;
+        return pq.size();
     }
 }
